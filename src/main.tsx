db6703b6ba21d45bd46e3df8c5031860687ac078
varun-developer-public <varundev.bytes@gmail.com>
2026-02-14 16:51:50 +0530

    `Update index.html and main.tsx to change app title and favicon`

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import penguinImg from "./assets/eb43db30baf40ba1d1ee413a90ac8503189b0177.png";
+
+document.title = "pandapenguin";
+const existingIcon = document.querySelector<HTMLLinkElement>("link[rel='icon']");
+const favicon = existingIcon ?? document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/png";
+favicon.href = penguinImg;
+if (!existingIcon) document.head.appendChild(favicon);
 
   createRoot(document.getElementById("root")!).render(<App />);
   
